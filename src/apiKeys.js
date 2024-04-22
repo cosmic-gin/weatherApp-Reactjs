193e97315b928af3184cdeb413bd1bf7ef126b8a
Shrey <shrey1patel2@gmail.com>
2024-04-23 00:04:22 +0530

    Update API key in apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "49ffdd9fb7462ad0a6eec17390d08d67",
   base: "https://api.openweathermap.org/data/2.5/",
 };
